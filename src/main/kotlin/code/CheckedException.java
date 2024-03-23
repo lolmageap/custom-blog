52f6aa5d9089c0972943a7e883da2274bdea0214
lolmageap <ekxk1234@naver.com>
2024-03-23 22:34:54 +0900

    create checked exception example in java

---
+++ src/main/kotlin/code/CheckedException.java
@@ -0,0 +1,16 @@
+package code;
+
+import java.io.*;
+
+public class CheckedException {
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+
+        try(FileReader fileReader = new FileReader(file)) {
+            BufferedReader reader = new BufferedReader(fileReader);
+            reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
