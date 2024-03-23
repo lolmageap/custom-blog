dbea012db1343e60ce28edd6fb857f55dad10dbe
lolmageap <ekxk1234@naver.com>
2024-03-23 22:35:00 +0900

    create checked exception example in kotlin

---
+++ src/main/kotlin/code/CheckedExceptionV1.kt
@@ -0,0 +1,12 @@
+package code
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+fun main() {
+    val file = File("test.txt")
+    val fileReader = FileReader(file)
+    val reader = BufferedReader(fileReader)
+    reader.readLine()
+}
---
+++ src/main/kotlin/code/CheckedExceptionV2.kt
@@ -0,0 +1,13 @@
+package code
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+fun main() {
+    val file = File("test.txt")
+    FileReader(file).use { fileReader ->
+        val reader = BufferedReader(fileReader)
+        reader.readLine()
+    }
+}
