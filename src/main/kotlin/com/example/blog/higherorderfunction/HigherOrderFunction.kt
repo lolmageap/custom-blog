98c94dcbef31130a4de2ca3f3778c5ab963bfe76
lolmageap <ekxk1234@naver.com>
2024-03-24 22:09:23 +0900

    add multiply function

--- src/main/kotlin/com/example/blog/higherorderfunction/HigherOrderFunction.kt
+++ src/main/kotlin/com/example/blog/higherorderfunction/HigherOrderFunction.kt
@@ -3,6 +3,11 @@ package com.example.blog.higherorderfunction
 fun main() {
     calculate(10, 20, ::sum)
     calculate(10, 20) { a, b -> a * b }
+
+    val function: (Int) -> Int = multiply(10, 20)
+
+    val result = function.invoke(2)
+    require(result == 400)
 }
 
 fun calculate(a: Int, b: Int, function: (Int, Int) -> Int) {
@@ -12,4 +17,10 @@ fun calculate(a: Int, b: Int, function: (Int, Int) -> Int) {
 
 fun sum(a: Int, b: Int): Int {
     return a + b
+}
+
+fun multiply(a: Int, b: Int): (Int) -> Int {
+    return { c ->
+        c * a * b
+    }
 }
